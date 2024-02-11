42671b0f2b50e962e16758fbd45f5265040b3320
UmarMamatov <umarmamat03@gmail.com>
2024-02-11 22:04:56 +0600

    lesson2 added

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Modal from "./modal/Modal";
+import {useState} from "react";
+import Count from "./count/Count";
+import Input from "./input/Input";
 
 function App() {
+    const [show, setShow] = useState(false)
+    const handleShow = ()=>{
+        setShow(!show)
+    }
+
+
+    const [input,setInput]= useState('')
+    const onChangeInput = (event)=>{
+        setInput(event.target.value)
+    }
+    const color = ()=>{
+        if (input.length<5){
+            return 'green'
+        }else return 'red'
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          {
+              show && <Modal handleShow={handleShow}>
+              </Modal>
+          }
+          <button onClick={handleShow}>Open</button>
+          <Count/>
+          <p className='inputValue' style={{color: color()}}>{input}</p>
+          <Input  placeholder={'write'} onChangeInput={onChangeInput}/>
+      </>
+
   );
 }
 
